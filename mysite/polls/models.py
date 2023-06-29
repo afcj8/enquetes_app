46b8dedcc59a507acc6139ba64a40d9b09c30fd7
afcj8 <contatoantoniojunior8@gmail.com>
2023-06-28 21:53:06 -0300

    criação das classes de modelo

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,25 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Pergunta(models.Model):
+    pergunta_texto = models.CharField(max_length = 200)
+    data_pub = models.DateTimeField('Data de publicação')
+    def __str__(self):
+        return self.pergunta_texto
+    def pub_recentemente(self):
+        return self.data_pub >= timezone.now() - datetime.timedelta(days = 1)
+    def total_votos(self):
+        total = 0
+        for alt in self.alternativa_set.all():
+            total += alt.votos
+        return total
+
+class Alternativa(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete = models.CASCADE)
+    alternativa_texto = models.CharField(max_length = 200)
+    votos = models.IntegerField(default = 0)
+    def __str__(self):
+        return self.alternativa_texto
+    def porcentagem(self):
+        return (self.votos / self.pergunta.total_votos()) * 100
